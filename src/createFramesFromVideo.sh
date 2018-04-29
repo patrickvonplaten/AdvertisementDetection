087ad6391bb5cc1b34c0db570ae0eebdf75a3a59
pvp23 <patrick.v.platen@gmail.com>
2018-04-29 16:40:18 +0200

    added video to frame converter

---
+++ src/createFramesFromVideo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# example command
+# install ffmpeg
+# ./convertImages.sh $(grealpath ../ARD_13-03-16_18-36_Sportschau_TSV_MÃ¼nchen.mpg) images 0.005
+
+mpegVideoPath='/Users/patrickvonplaten/AdvertisementDetection/ARD_13-03-16_18-36_Sportschau_TSV_MÃ¼nchen.mpg'
+pathToSaveImages='/Users/patrickvonplaten/AdvertisementDetection/images'
+picturesPerSecond=${1} #0.005 for example will result in 3 frames for videofile
+
+currentPath=$(pwd)
+if [ -f ${pathToSaveImages} ]; then
+	rm -r ${pathToSaveImages} 
+fi
+
+mkdir ${pathToSaveImages}
+cd ${pathToSaveImages}
+ffmpeg -i ${mpegVideoPath} -r ${picturesPerSecond} image-%04d.jpeg
+cd ${currentPath}
+
