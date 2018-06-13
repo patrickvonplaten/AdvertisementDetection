f568f1936bca013f473579ed537ec9a99cb13789
esinkoyuncu <koyuncuesin@gmail.com>
2018-06-13 19:16:10 +0200

    q

--- src/findAdvertisementLocation.py
+++ src/findAdvertisementLocation.py
@@ -3,10 +3,11 @@ import cv2
 import tensorflow as tf
 from preprocessor import Preprocessor
 from tensorflow.python.keras.applications import vgg16
-from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.keras.models import *
+from tensorflow.python.keras.callbacks import *
 from tensorflow.python.keras.layers import Flatten, Dense
-import matplotlib.pyplot as plt
-#from matplotlib import pyplot as plt  #python -mpip install -U matplotlib
+from tensorflow.python.keras import backend as K
+
 
 #
 # INPUTS FOR TESTING_____________
@@ -20,7 +21,7 @@ for layer in _model.layers:
     model.add(layer)
 model.add(Flatten(name='flatten'))
 model.add(Dense(1000, activation='softmax', kernel_initializer='random_uniform',bias_initializer='random_uniform'))
-model.add(Dense(2, activation='softmax', kernel_initializer='random_uniform',bias_initializer='random_uniform'))
+model.add(Dense(1, activation='softmax', kernel_initializer='random_uniform',bias_initializer='random_uniform'))
 model.summary()
 # _____________________________
 #
@@ -35,61 +36,74 @@ class findAdvLocation(object):
 
     def getLayerOutput(self, input_image, layer_no):
         intermediate_layer_model = Model(inputs=self.model.input, outputs=self.model.get_layer(index = layer_no).output)
-        intermediate_output = intermediate_layer_model.predict(input_image)
+        intermediate_output, predictions = intermediate_layer_model.predict(input_image)
         return intermediate_output
 
 
-    def drawGridOnImg(self):
+    def drawGridOnAdvRegion(self):
+        img = obj_findAdvLocation.testData[0]
+        width, height = 32,32
+        R, G, B = (0,0,255), (0,255,0), (255,0,0)
+        #M = 18x24 output activations matrix -- normalize
+        M = np.ones((18,24))
+        for i in range(m):
+            for j in range(n):
+                if(M[i][j]>0.7):
+                    cv2.rectangle(img, (j*width,i*height), ((j+1)*width,(i+1)*height), G)
+        cv2.imshow('result', img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
         pass
-    def showImageOnScreen(self,image):
-        cv2.imshow('video',image)
-        cv2.waitkey(0)
+
     def tracebackNetwork(self):
-        testInput = obj_findAdvLocation.testData
+        testInput = self.testData
         threshold = 0.7
-        guess = obj_findAdvLocation.getLayerOutput(input_image = testInput, layer_no = -1)[0][1]
+        #guess = self.getLayerOutput(input_image = testInput, layer_no = -1)[0][1]
         guess = 1
         print(guess)
-        if(guess>threshold):
-            last_activations = obj_findAdvLocation.getLayerOutput(input_image = testInput, layer_no = -2)
-            previous_activations = obj_findAdvLocation.getLayerOutput(input_image = testInput, layer_no = -4)
-            print('PRINTING ABOUT ACTIVATIONS ---------------------- \n ')
-            print('last layer shape', last_activations.shape)
-            print('prev layer shape', previous_activations.shape)
-            print('element 5,5', previous_activations[0].shape)
-            K=1
-            indices = np.argpartition(previous_activations,-K,axis=-3)[-K:]
-            print('indices ', indices.shape)
+        img = self.testData
+        self.model.summary()
 
 
 
+        class_weights = self.model.layers[-1].get_weights()[0]
+        final_dense_layer = self.model.get_layer(index = -2).output
+        get_output = K.function([self.model.layers[0].input], [final_dense_layer,self.model.get_layer(index = -1).output])
+        [dense_outputs, predictions] = get_output([img])
+        print("outputs shape",dense_outputs.shape)
+        print("weights shape",class_weights.shape)
+        cam = np.multiply(dense_outputs,np.transpose(class_weights))
+        print("cam shape",cam.shape)
+        cam /= np.max(cam)
+        max_contribution_index = np.argmax(cam)
+        print("max index",max_contribution_index)
 
+        "repeat for dense-to-flatten"
+        class_weights = self.model.layers[-2].get_weights()[0]
+        flatten_layer = self.model.get_layer(index = -3).output
+        get_output = K.function([self.model.layers[0].input], [flatten_layer,self.model.get_layer(index = -2).output])
+        [flatten_outputs, predictions] = get_output([img])
+        print("outputs shape",flatten_outputs.shape)
+        print("weights shape",class_weights.shape)
+        column_for_max_index = class_weights[:,max_contribution_index]
+        cam = np.multiply(flatten_outputs,np.transpose(column_for_max_index))
+        print("cam shape",cam.shape)
 
+        cam /= np.max(cam)
+        print(cam)
+        max_contribution_index = np.argmax(cam)
+        print("max index",max_contribution_index)
 
-
+        # if(guess>threshold):
+        #     last_activations = obj_findAdvLocation.getLayerOutput(input_image = testInput, layer_no = -2)
+        #     previous_activations = obj_findAdvLocation.getLayerOutput(input_image = testInput, layer_no = -4)
+        #     print('PRINTING ABOUT ACTIVATIONS ---------------------- \n ')
+        #     print('last layer shape', last_activations.shape)
+        #     print('prev layer shape', previous_activations.shape)
+        #     print('element 5,5', previous_activations[0].shape)
+        #     K=1
+        #     indices = np.argpartition(previous_activations,-K,axis=-3)[-K:]
+        #     print('indices ', indices.shape)
 
 obj_findAdvLocation = findAdvLocation(testData = _data.testData,model = model)
 obj_findAdvLocation.tracebackNetwork()
-
-
-
-
-
-
-
-img = obj_findAdvLocation.testData[0]
-#cv2.imshow('result',obj_findAdvLocation.testData[0])
-# dx, dy = 10,10
-#
-# grid_color = -1
-# img[:,::dy] = grid_color
-# img[::dx,:] = grid_color
-
-width, height = 32,32
-R, G, B = (0,0,255), (0,255,0), (255,0,0)
-
-cv2.rectangle(img, (0,0), (width,height), R)
-cv2.imshow('result', img)
-#plt.show()
-cv2.waitKey()
-cv2.destroyAllWindows()
